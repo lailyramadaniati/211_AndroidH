edc59867c489f1bc02b6c1723205b18aa82fe8bb
lailyramadaniati <lailyramadaniati159@gmail.com>
2021-11-28 12:50:33 +0800

    Login, SharedPreferences, Logout

--- app/src/main/java/com/lailyramadaniati/androidh211/MainActivity.kt
+++ app/src/main/java/com/lailyramadaniati/androidh211/MainActivity.kt
@@ -14,12 +14,13 @@ class MainActivity : AppCompatActivity() {
 
         val simpanButton = findViewById<Button>(R.id.simpanButton)
         val batalButton = findViewById<Button>(R.id.batalButton)
+        val logoutButton = findViewById<Button>(R.id.logoutButton)
 
         val namaEditText = findViewById<EditText>(R.id.namaEditText)
         val emailEditText = findViewById<EditText>(R.id.emailEditText)
         val noHandphoneEditText = findViewById<EditText>(R.id.noHandphoneEditText)
         val jarakEditText = findViewById<EditText>(R.id.jarakEditText)
-        val tanggalLahirEditText = findViewById<EditText>(R.id.jarakEditText)
+        val tanggalLahirEditText = findViewById<EditText>(R.id.tanggalLahirEditText)
 
         simpanButton.setOnClickListener{
             val nama = namaEditText.text.toString()
@@ -42,6 +43,20 @@ class MainActivity : AppCompatActivity() {
 
         batalButton.setOnClickListener {
             finish()
+        }
+
+        logoutButton.setOnClickListener {
+            val sharedPreferences = this.getSharedPreferences("MY_SP", MODE_PRIVATE)
+
+            with(sharedPreferences.edit()) {
+                putString("username", "")
+                putString("password", "")
+                apply()
+            }
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
